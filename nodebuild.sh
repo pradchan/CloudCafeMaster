73fd2c0760a175deace4189139fba8242b8a907a
PRADCHAN.IN <pradeep.chandramouli@oracle.com>
2017-02-01 15:33:46 +0530

    For Node.js jobs

---
+++ nodebuild.sh
@@ -0,0 +1,15 @@
+npm config set proxy http://dmz-proxy-adcq7.us.oracle.com:80
+
+npm config set https-proxy http://dmz-proxy-adcq7.us.oracle.com:80
+
+npm install -g express
+
+npm install express-generator -g
+
+express oraclejetwithnodejs
+
+cd oraclejetwithnodejs
+
+npm install
+
+
